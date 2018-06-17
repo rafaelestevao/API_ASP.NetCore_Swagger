13dcdbea1a37c3d29d662f1fd0caae123873d1c6
rafaelestevao <rafael-estevao@hotmail.com>
2018-06-17 13:52:22 -0300

    +add

---
+++ ApiSwaggerTest/Controllers/ValuesController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiSwaggerTest.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+
+
+        public class Funcionario
+        {
+            public string Nome { get; set; }
+            public string Endereco { get; set; }
+            public string Cidade { get; set; }
+            public string Estado { get; set; }
+            public string telefone { get; set; }
+        }
+
+
+        // GET api/values
+        [HttpGet]
+        public Funcionario Get()
+        {
+            //return new string[] { "value1", "value2" };
+            return new Funcionario() { Nome = "Rafael Estevão", Endereco = "Rua armelindo cruz, 322", Cidade = "São Paulo", Estado = "SP", telefone = "11-0000-0000" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ApiSwaggerTest/Startup.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace ApiSwaggerTest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "ApiSwaggerTest",
+                        Version = "v1",
+                        Description = "Teste API ASP.Net Core com swagger",
+                        Contact = new Contact
+                        {
+                            Name = "Rafael Estevão",
+                            Url = "https://github.com/rafaelestevao"
+                        }
+                    });
+            });
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = "swagger";
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiSwaggerTest");
+            });
+
+            app.UseMvc();
+        }
+    }
+}
